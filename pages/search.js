4022e9a34c24194f06327f29a5062fbc66dfff5f
soojinpark99 <tnwls7263@gmail.com>
2024-05-09 17:21:30 +0900

    initial commit

---
+++ pages/search.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ProductList from "@/components/ProductList";
+import SearchForm from "@/components/SearchForm";
+import axios from "@/lib/axios";
+import styles from "@/styles/Search.module.css";
+
+export default function Search() {
+  const [products, setProducts] = useState([]);
+  const router = useRouter();
+  const { q } = router.query;
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
+
+  return (
+    <div>
+      <SearchForm initialValue={q} />
+      <h2 className={styles.title}>
+        <span className={styles.keyword}>{q}</span> 검색 결과
+      </h2>
+      <ProductList className={styles.productList} products={products} />
+    </div>
+  );
+}
---
+++ pages/setting.js
@@ -0,0 +1,12 @@
+import styles from "@/styles/Setting.module.css";
+
+export default function Setting() {
+  return (
+    <>
+      <h1 className={styles.title}>설정</h1>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>테마 설정</h2>
+      </section>
+    </>
+  );
+}
